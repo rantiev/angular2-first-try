caebca389d03bd26e1d10193b836463242ba2200
Pavel <rantie86@gmail.com>
2017-08-15 05:21:56 +0300

    First changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  listItems = [
+    {
+      name: 'Baba Jaba',
+      quantity: 10,
+    },
+    {
+      name: 'Haba Haba',
+      quantity: 20,
+    },
+    {
+      name: 'Paca Vaca',
+      quantity: 30,
+    },
+  ]
+
+  btnActive = true
+
+  resort() {
+    this.listItems = this.listItems
+    .sort((a, b) => b.quantity - a.quantity)
+
+    this.btnActive = false
+  }
 }
