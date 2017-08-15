ac0af0e218b1862a5e42036f26a7452cac570a45
Pavel <rantie86@gmail.com>
2017-08-15 20:56:38 +0300

    Implement simple to do app using angular material styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,27 +8,67 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
-  listItems = [
+  newToDo = {
+    title: null,
+    completed: false,
+  }
+
+  toDoList = [
     {
-      name: 'Baba Jaba',
-      quantity: 10,
+      title: 'Task #1',
+      completed: false,
     },
     {
-      name: 'Haba Haba',
-      quantity: 20,
+      title: 'Task #2',
+      completed: false,
     },
     {
-      name: 'Paca Vaca',
-      quantity: 30,
+      title: 'Task #3',
+      completed: false,
     },
   ]
 
+  completedList = []
+  hasNotArchivedCompleted = false
+
   btnActive = true
 
-  resort() {
-    this.listItems = this.listItems
-    .sort((a, b) => b.quantity - a.quantity)
+  complete (item) {
+    item.completed = !item.completed;
 
-    this.btnActive = false
+    this.hasNotArchivedCompleted = true
+  }
+
+  archiveCompleted() {
+    let completedItems = this.toDoList.filter(item => item.completed)
+
+    this.completedList = this.completedList.concat(completedItems);
+
+    this.toDoList = this.toDoList.filter(item => !item.completed)
+
+    this.hasNotArchivedCompleted = false
+  }
+
+  unarchive (item) {
+    item.completed = !item.completed;
+
+    this.completedList = this.completedList.filter(_item => _item !== item)
+    this.toDoList = this.toDoList.concat(item);
+  }
+
+  addToDo () {
+    if (this.toDoList.indexOf(this.newToDo) === -1 &&
+      this.completedList.indexOf(this.newToDo) === -1) {
+      this.toDoList = this.toDoList.concat(this.newToDo)
+    }
+
+    this.newToDo = {
+      title: null,
+      completed: false,
+    }
+  }
+
+  edit (item) {
+    this.newToDo = item;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import {
+  MdInputModule,
+  MdListModule,
+  MdButtonModule,
+  MdCheckboxModule } from '@angular/material';
+
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from './app.component';
 
+import 'hammerjs';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    FlexLayoutModule,
+    MdInputModule,
+    MdListModule,
+    MdButtonModule,
+    MdCheckboxModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
